9fdc36f67e75b8c542f634fb4e6cb37beee52246
justinkunz <justin_kunz@yahoo.com>
2019-12-26 12:56:51 -0600

    added allow toggle to add new ipsums route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const db = require("../controllers/ipsums");
-
+const { ALLOW_NEW_ENTERIES } = process.env;
 module.exports = app => {
   // Get characters[] route
   app.get("/api/characters", async (req, res) => {
@@ -8,17 +9,22 @@ module.exports = app => {
 
   // Save new ipsum route
   app.post("/api/new", async (req, res) => {
-    res.json(await db.create(req.body));
+    if (ALLOW_NEW_ENTERIES) {
+      res.json(await db.create(req.body));
+    } else {
+      res.json({ status: "error", msg: "No new enteries allowed" });
+    }
   });
 
   // Get Ipsums on condition route
   app.post("/api/ipsums", async (req, res) => {
-    const { choosen } = req.body;
+    const { choosen, limit, nsfw } = req.body;
     const chars = Object.keys(choosen).filter(sel => choosen[sel]);
 
     const conditions = {
       choosen: chars,
-      limit: 10
+      limit,
+      nsfw
     };
 
     res.json(await db.findIpsums(conditions));
