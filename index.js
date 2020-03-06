63624a6d0bf29f1d206f90fb003c0e030798e5b4
justinkunz <justin_kunz@yahoo.com>
2020-03-05 20:54:30 -0600

    added path to index

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ require("dotenv").config();
 require("./dbConfig");
 
 const express = require("express");
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
 
 const routes = require("./routes");
+const { NODE_ENV } = process.env;
 
 const app = express();
-
-app.use([
-  express.urlencoded({ extended: true }),
-  express.json()
-  //   createProxyMiddleware("/api/*", { target: "localhost:3001" })
-]);
 const PORT = process.env.PORT || 3001;
 
+app.use([express.urlencoded({ extended: true }), express.json()]);
+
+if (NODE_ENV === "production") {
+  console.log("YOU ARE IN THE PRODUCTION ENV");
+  app.use("/static", express.static(path.join(__dirname, "../build/static")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/"));
+  });
+}
 routes(app);
 app.listen(PORT);
