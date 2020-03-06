0464341053a0823152e0e1c344023452dc75cba6
justinkunz <justin_kunz@yahoo.com>
2020-03-05 20:51:02 -0600

    added env dev var

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ require("dotenv").config();
 require("./dbConfig");
 
 const express = require("express");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const routes = require("./routes");
+
 const app = express();
 
-app.use([express.urlencoded({ extended: true }), express.json()]);
+app.use([
+  express.urlencoded({ extended: true }),
+  express.json()
+  //   createProxyMiddleware("/api/*", { target: "localhost:3001" })
+]);
 const PORT = process.env.PORT || 3001;
 
 routes(app);
