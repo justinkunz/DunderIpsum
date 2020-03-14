bf7188d7be399dbf24658aa01aa97183196c3781
justinkunz <justin_kunz@yahoo.com>
2020-03-14 13:21:07 -0500

    seperated class - added favicon

---
+++ classes/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  IpsumSet: require("./IpsumSet")
+};
--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,37 +1,5 @@
 const { Quotes } = require("../models");
-
-class IpsumSet {
-  constructor(dbResults) {
-    this.quotes = dbResults.map(ip => ip.quote);
-  }
-
-  shuffle() {
-    for (let i = 0; i < this.quotes.length; i++) {
-      const target = this.quotes[i];
-      const swapIndex = Math.floor(Math.random() * this.quotes.length);
-      this.quotes[i] = this.quotes[swapIndex];
-      this.quotes[swapIndex] = target;
-    }
-    return this;
-  }
-
-  makeParagraphs(paraCharLimit, max) {
-    const arr = this.quotes;
-    const para = [];
-    let i = 0;
-
-    while (i < arr.length && para.length < max) {
-      let combinedIpsumStr = arr[i];
-      i++;
-      while (combinedIpsumStr.length < paraCharLimit && i < arr.length) {
-        combinedIpsumStr += ` ${arr[i]}`;
-        i++;
-      }
-      para.push(combinedIpsumStr);
-    }
-    return para;
-  }
-}
+const { IpsumSet } = require("../classes");
 
 module.exports = {
   /**
